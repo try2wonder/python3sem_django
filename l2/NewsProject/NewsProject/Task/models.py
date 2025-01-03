1a3f2659a0d27ec8b13acc4272aae4816079bc47
try2wonder <ilja.jan@yandex.ru>
2025-01-03 19:20:02 +0300

    messing around

--- l2/NewsProject/NewsProject/Task/models.py
+++ l2/NewsProject/NewsProject/Task/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Human(models.Model):
+    name = models.CharField(max_length=30)
+    age = models.IntegerField()
+    gender = models.Choices('male','female')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    photo = models.ImageField(upload_to='media/%Y/%m/%d')
+    is_published = models.BooleanField(default=True)
